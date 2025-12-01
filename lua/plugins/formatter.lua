30a6418a3a3a3b46662f46482f31ac630d54ffce
leifkeidl <keidll@uwplatt.edu>
2025-12-01 14:04:38 -0600

    Python formatting

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -7,45 +7,61 @@ function M.setup()
 
   conform.setup({
     formatters_by_ft = {
+      -- Lua
       lua = { "stylua" },
-      svelte = { { "prettierd", "prettier", stop_after_first = true } },
-      astro = { { "prettierd", "prettier", stop_after_first = true } },
-      javascript = { { "prettierd", "prettier", stop_after_first = true } },
-      typescript = { { "prettierd", "prettier", stop_after_first = true } },
-      javascriptreact = { { "prettierd", "prettier", stop_after_first = true } },
-      typescriptreact = { { "prettierd", "prettier", stop_after_first = true } },
-      json = { { "prettierd", "prettier", stop_after_first = true } },
-      graphql = { { "prettierd", "prettier", stop_after_first = true } },
+
+      -- Python: try ruff_format, then black
+      python = { "ruff_format", "black" },
+
+      -- Rust
+      rust = { "rustfmt" },
+
+      -- JS / TS / web stuff
+      svelte = { "prettierd", "prettier" },
+      astro = { "prettierd", "prettier" },
+      javascript = { "prettierd", "prettier" },
+      typescript = { "prettierd", "prettier" },
+      javascriptreact = { "prettierd", "prettier" },
+      typescriptreact = { "prettierd", "prettier" },
+      json = { "prettierd", "prettier" },
+      graphql = { "prettierd", "prettier" },
+      markdown = { "prettierd", "prettier" },
+      css = { "prettierd", "prettier" },
+      scss = { "prettierd", "prettier" },
+
+      -- Other languages you had
       java = { "google-java-format" },
       kotlin = { "ktlint" },
       ruby = { "standardrb" },
-      markdown = { { "prettierd", "prettier", stop_after_first = true } },
       erb = { "htmlbeautifier" },
       html = { "htmlbeautifier" },
       bash = { "beautysh" },
       proto = { "buf" },
-      rust = { "rustfmt" },
       yaml = { "yamlfix" },
       toml = { "taplo" },
-      css = { { "prettierd", "prettier", stop_after_first = true } },
-      scss = { { "prettierd", "prettier", stop_after_first = true } },
-      sh = { "shellcheck" },
+      sh = { "shfmt" },      -- formatter, not shellcheck
       go = { "gofmt" },
       xml = { "xmllint" },
+
+      -- Fallback for everything else
+      ["*"] = { "trim_newlines", "trim_whitespace" },
     },
   })
 
-  -- Actual formatting keymap: <Space> c f
+  -- <Space> c f: format with Conform
   vim.keymap.set({ "n", "v" }, "<leader>cf", function()
     conform.format({
       lsp_fallback = true,
       async = false,
       timeout_ms = 2000,
+      stop_after_first = true,  -- first available formatter wins
     })
   end, { desc = "Format buffer or selection" })
 end
 
+-- Either call setup here...
 M.setup()
 
 return M
+
 
